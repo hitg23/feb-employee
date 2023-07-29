5c9bd02320dcbc5576a2bbbe866590ddfe25d10e
hitg23 <58311785+hitg23@users.noreply.github.com>
2023-07-29 10:24:10 -0400

    added styled componenet nedd to fix later

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Container.css'
+import {Styled} from 'styled.component'
+const StyledContainer = Styled.Container`
 
+`
 function Container({children}) {
   return (
+   
     <div className='container'>{children}</div>
+    
   )
 }
 
